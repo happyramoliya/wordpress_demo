71b913d04111879eeb527acb546da222abeeb3d3
happyramoliya <happyramoliya45187@gmail.com>
2023-09-22 15:07:53 +0530

    apidemo template

--- wp-content/plugins/custom-api-settings/settings-page.php
+++ wp-content/plugins/custom-api-settings/settings-page.php
@@ -1,13 +1,13 @@
-<!-- <div class="wrap">
+<div class="wrap">
     <h1>API Settings</h1>
     <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
         <input type="hidden" name="action" value="save_api_settings">
         <label for="api_key">API Key</label>
         <input type="text" name="api_key" id="api_key"
-            value="<?php //echo esc_attr(get_option('api_key', 'a74f0a6d-780f-4c4e-b600-5af9eb1e6775')); ?>" />
-        <?php //submit_button(); ?>
+            value="<?php echo esc_attr(get_option('api_key', 'a74f0a6d-780f-4c4e-b600-5af9eb1e6775')); ?>" />
+        <?php submit_button(); ?>
     </form>
-</div> -->
+</div>
 
 <?php
 // Code for API Display
