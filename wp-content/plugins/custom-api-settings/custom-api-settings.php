497e0b5183bceeb665156eb513c276f8700b4233
happyramoliya <happyramoliya45187@gmail.com>
2023-09-22 17:58:23 +0530

    api settings page colde change

--- wp-content/plugins/custom-api-settings/custom-api-settings.php
+++ wp-content/plugins/custom-api-settings/custom-api-settings.php
@@ -26,4 +26,39 @@ function api_settings_page() {
 add_action('admin_menu', 'add_admin_menu');
 
 
+// Code for API Display
+function display_api_data() {
+    $url = "https://dummy.restapiexample.com/api/v1/employees";
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    if ($response) {
+        $employees_data = json_decode($response, true);
+
+        if (isset($employees_data)) {
+            echo '<div id="api-data">';
+            echo '<h2>Employees Data</h2>';
+            echo '<ul>';
+
+            foreach ($employees_data['data'] as $employee) {
+                echo '<li>';
+                echo '<strong>Name:</strong> ' . $employee['employee_name'] . ', ';
+                echo '<strong>Salary:</strong> ' . $employee['employee_salary'] . ', ';
+                echo '<strong>Age:</strong> ' . $employee['employee_age'];
+                echo '</li>';
+            }
+
+            echo '</ul>';
+            echo '</div>';
+        } else {
+            echo '<p>No employee data available.</p>';
+        }
+    }
+}
+
+// Add a shortcode to display the API data
+add_shortcode('display_api_data', 'display_api_data');
 
--- wp-content/plugins/custom-api-settings/settings-page.php
+++ wp-content/plugins/custom-api-settings/settings-page.php
@@ -1,3 +1,10 @@
+<?php
+
+//Template Name: API Demo
+
+get_header();
+?>
+
 <div class="wrap">
     <h1>API Settings</h1>
     <form method="post" action="<?php echo admin_url('admin-post.php'); ?>">
@@ -9,44 +16,11 @@
     </form>
 </div>
 
+<?php echo do_shortcode('[display_api_data]'); ?>
+
 <?php
-// Code for API Display
-function display_api_data() {
-    $url = "https://dummy.restapiexample.com/api/v1/employees";
-
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    if ($response) {
-        $employees_data = json_decode($response, true);
-
-        if (isset($employees_data)) {
-            echo '<div id="api-data">';
-            echo '<h2>Employees Data</h2>';
-            echo '<ul>';
-
-            foreach ($employees_data['data'] as $employee) {
-                echo '<li>';
-                echo '<strong>Name:</strong> ' . $employee['employee_name'] . ', ';
-                echo '<strong>Salary:</strong> ' . $employee['employee_salary'] . ', ';
-                echo '<strong>Age:</strong> ' . $employee['employee_age'];
-                echo '</li>';
-            }
-            echo '</ul>';
-            echo '</div>';
-        } else {
-            echo '<p>No employee data available.</p>';
-        }
-    }
-}
-
-// Add a shortcode to display the API data
-add_shortcode('display_api_data', 'display_api_data');
-
-
-
+get_footer();
+?>
 
 
 
