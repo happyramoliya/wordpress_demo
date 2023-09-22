68859634792e1c198dbf46f6ff375c379404f390
happyramoliya <happyramoliya45187@gmail.com>
2023-09-22 12:46:25 +0530

    settings api

--- wp-content/plugins/custom-api-settings/api-request.php
+++ wp-content/plugins/custom-api-settings/api-request.php
@@ -1,10 +1,14 @@
 <?php
-$api_key = get_option('api_key', 'abcdef1234567890');
+$api_key = get_option('api_key', 'a74f0a6d-780f-4c4e-b600-5af9eb1e6775');
 
-$url = "https://api.example.com/weather?api_key=$api_key&location=New+York";
+$url = "https://dummy.restapiexample.com/api/v1/employees?api_key=$api_key";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
 curl_close($ch);
-    
+
+if ($response) {
+    $weather_data = json_decode($response, true);
+}
+?>
--- wp-content/plugins/custom-api-settings/settings-page.php
+++ wp-content/plugins/custom-api-settings/settings-page.php
@@ -4,7 +4,7 @@
         <input type="hidden" name="action" value="save_api_settings">
         <label for="api_key">API Key</label>
         <input type="text" name="api_key" id="api_key"
-            value="<?php echo esc_attr(get_option('api_key', 'abcdef1234567890')); ?>" />
+            value="<?php echo esc_attr(get_option('api_key', 'a74f0a6d-780f-4c4e-b600-5af9eb1e6775')); ?>" />
         <?php submit_button(); ?>
     </form>
 </div>
