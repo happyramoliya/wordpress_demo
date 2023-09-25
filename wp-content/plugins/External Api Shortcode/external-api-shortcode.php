9c86e337dcb2011ae3ed829ce0b1b838f536842a
happyramoliya <happyramoliya45187@gmail.com>
2023-09-25 16:23:02 +0530

    external api

--- wp-content/plugins/External Api Shortcode/external-api-shortcode.php
+++ wp-content/plugins/External Api Shortcode/external-api-shortcode.php
@@ -1,10 +1,8 @@
 <?php
 /**
  * Plugin name: External Api Shortcode
- * Plugin URI: https://omukiguy.com
  * Description: Get information from external APIs in WordPress
- * Author: Laurence Bahiirwa
- * Author URI: https://omukiguy.com
+ * Author: Happy Patel
  * version: 0.1.0
  * License: GPL2 or later.
  * text-domain: prefix-plugin-name
@@ -14,14 +12,14 @@
 defined('ABSPATH')  || die('Unauthorized Access');
 
 // Action when user logs into admin panel
-add_shortcode('external_data', 'callback_function_name');
+add_shortcode('external_data', 'callback_function');
 
-function callback_function_name( $atts ) {
+function callback_function( $atts ) {
 
 	if ( is_admin() ) {
 		return '<p>This is where the shortcode [external_data] will show up.</p>';
-	}
-
+  	}
+  
     $defaults = [
       'title'  => 'Table title'
     ];
