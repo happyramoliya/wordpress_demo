44c5b1b138c45b6e6d8d5d9df74c90fe814f8fff
happyramoliya <happyramoliya45187@gmail.com>
2023-09-29 19:31:29 +0530

    ajax new file

--- wp-content/themes/wordpress-demo/js/script.js
+++ wp-content/themes/wordpress-demo/js/script.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    $('#load-data').click(function() {
-        $.ajax({
+jQuery(document).ready(function() {
+    jQuery('#load-data').click(function() {
+        jQuery.ajax({
             url: 'https://jsonplaceholder.typicode.com/posts',
             type: 'GET',
             dataType: 'json',
@@ -14,7 +14,7 @@ $(document).ready(function() {
     });
 
     function displayData(data) {
-        var container = $('#data-container');
+        var container = jQuery('#data-container');
         container.empty();
         data.forEach(function(post) {
             container.append('<div><h3>' + post.title + '</h3><p>' + post.body + '</p></div>');
