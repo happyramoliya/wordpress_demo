778bd63a93493ebbdf385be02d4bf8acca169aa0
happyramoliya <happyramoliya45187@gmail.com>
2023-09-29 18:55:08 +0530

    ajax new file

---
+++ wp-content/themes/wordpress-demo/page-templates/demo.php
@@ -0,0 +1,8 @@
+<?php
+//Template Name: ajax demo
+get_header();
+?>
+<h1>AJAX Demo</h1>
+<button id="load-data">Load Data</button>
+<div id="data-container"></div>
+<!-- <script src="script.js"></script> -->
