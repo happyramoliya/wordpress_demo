5fc5f306601327d16dcce4ad10f2f15b3f479bcb
happyramoliya <happyramoliya45187@gmail.com>
2023-09-29 11:25:20 +0530

    config file add allow multisite

--- wp-config.php
+++ wp-config.php
@@ -83,6 +83,7 @@ define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 define('FS_METHOD', 'direct');
+define('WP_ALLOW_MULTISITE', true);
 
 
 /* That's all, stop editing! Happy publishing. */
@@ -94,3 +95,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+
+
+
+
+
+
+
+
+
