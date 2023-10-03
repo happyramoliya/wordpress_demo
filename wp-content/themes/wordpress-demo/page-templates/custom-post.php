b9bd79b53826ad499a0c8dd1164f06fdea212d27
happyramoliya <happyramoliya45187@gmail.com>
2023-10-03 16:54:39 +0530

    custom-style

--- wp-content/themes/wordpress-demo/page-templates/custom-post.php
+++ wp-content/themes/wordpress-demo/page-templates/custom-post.php
@@ -20,6 +20,7 @@ get_header();
                 </div>
                 <div class="blog-list-content">
                     <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+                    <p><?php the_excerpt();?></p>
                 </div>
             </div>
             <?php endwhile;?>
