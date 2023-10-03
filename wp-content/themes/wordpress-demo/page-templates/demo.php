1b041df6889a72b94096891442b321308efcdb40
happyramoliya <happyramoliya45187@gmail.com>
2023-10-03 13:19:14 +0530

    ajax load data demo

--- wp-content/themes/wordpress-demo/page-templates/demo.php
+++ wp-content/themes/wordpress-demo/page-templates/demo.php
@@ -3,10 +3,11 @@
 wp_head();
 ?>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- <script src="your_script.js"></script> -->
 <h1>AJAX Demo</h1>
 <button id="load-data">Load Data</button>
 <div id="data-container"></div>
-<!-- <script src=""></script> -->
 
 <?php
 wp_footer();
